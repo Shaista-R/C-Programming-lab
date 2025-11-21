813fc3dcea34287ca4dcfc5200bd6703ef68df30
Shaista-R <meb25003@tezu.ac.in>
2025-11-21 22:40:00 +0530

    Add files via upload

---
+++ lab1/introduction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Anjum Shaista Rahman.\n");
+    printf("My branch is Mechanical Engineering.\n");
+    printf("My roll no is MEB25003.\n");
+
+return 0;
+}
